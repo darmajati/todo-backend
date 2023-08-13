5768cd2af69982659ce1d269e01283c5ef1e235e
Pranowo Darmajati <darmajati45@gmail.com>
2023-08-13 23:00:04 +0700

    resolve config.js

--- config/config.js
+++ config/config.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 module.exports = {
   "development": {
+    "url": process.env.DATABASE_URL,
     "username": process.env.DB_USERNAME_DEV,
     "password": process.env.DB_PASSWORD_DEV,
     "database": process.env.DB_DEV,
-    "host": "127.0.0.1",
+    "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
   "test": {
